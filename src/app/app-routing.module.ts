51240b1d969ec387abf259ab6dc11490f6209b2d
samir28 <samirallegrucci@gmail.com>
2018-08-26 10:55:10 -0300

    add .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,6 @@
-import { NgModule } from '@angular/core';
-import {Routes, RouterModule} from '@angular/router';
-import { PortafolioComponent } from './pages/portafolio/portafolio.component';
-import { AboutComponent } from './pages/about/about.component';
-import { ItemComponent } from './pages/item/item.component';
 
 
-const app_routes: Routes = [
-  {path: '', component: PortafolioComponent},
-  {path: 'about', component: AboutComponent},
-  {path: 'item', component: ItemComponent},
-  {path: '**', pathMatch: 'full', redirectTo: ''}
-];
+@NgModule
+export class AppRouting{
 
-@NgModule({
-    imports: [
-        RouterModule.forRoot(app_routes, { useHash: true })
-    ],
-
-    exports: [RouterModule],
-})
-
-export class AppRoutingModule { }
+}
