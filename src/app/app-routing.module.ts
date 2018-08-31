2f19c386b2bd61df86f520afd9cf5f38a3ae6bdc
samir28 <samirallegrucci@gmail.com>
2018-08-31 10:54:36 -0300

    add .

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,3 +1,28 @@
-@NgModule
+import { ItemComponent } from './pages/item/item.component';
+import { AboutComponent } from './pages/about/about.component';
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { PortafolioComponent } from './pages/portafolio/portafolio.component';
+
+
+const app_routes: Routes = [
+
+  { path: '', component: PortafolioComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'item', component: ItemComponent },
+  { path: '**', pathMatch: 'full', redirectTo: '' }
+];
+
+
+@NgModule({
+    imports: [
+      RouterModule.forRoot( app_routes )
+    ],
+    exports: [
+      RouterModule
+    ]
+
+})
 export class AppRouting {
+
 }
